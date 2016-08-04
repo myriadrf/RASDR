be6c9f3f06fe1ea91e99d13e5eed211536c8fe6c
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2016-08-04 06:24:49 -0400

    tag for fix to RSS spectrum

--- host/RASDRproc/src/version.h
+++ host/RASDRproc/src/version.h
@@ -34,8 +34,8 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "07";
-	static const char MONTH[] = "30";
+	static const char DATE[] = "08";
+	static const char MONTH[] = "04";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] = "13.10";
 
@@ -47,13 +47,13 @@ namespace AutoVersion{
 	static const long MAJOR = 0;
 	static const long MINOR = 2;
 	static const long BUILD = 3;
-	static const long REVISION = 2;
+	static const long REVISION = 3;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 1;
-	#define RC_FILEVERSION 0,2,3,2
-	#define RC_FILEVERSION_STRING "0, 2, 3, 2\0"
-	static const char FULLVERSION_STRING[] = "0.2.3.2";
+	#define RC_FILEVERSION 0,2,3,3
+	#define RC_FILEVERSION_STRING "0, 2, 3, 3\0"
+	static const char FULLVERSION_STRING[] = "0.2.3.3";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 33;
