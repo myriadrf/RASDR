a6746393fdd125ba8a623d0894fc7e7f3c66fa6b
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2016-10-22 08:30:15 -0400

    update notes for v0.2.3.8 release

--- host/RASDRproc/src/version.h
+++ host/RASDRproc/src/version.h
@@ -35,12 +35,12 @@ namespace AutoVersion{
 
 	//Date Version Types
 	static const char DATE[] = "10";
-	static const char MONTH[] = "02";
+	static const char MONTH[] = "22";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] = "13.10";
 
 	//Software Status
-	static const char STATUS[] = "Working Set";
+	static const char STATUS[] = "Alpha";
 	static const char STATUS_SHORT[] = "";
 
 	//Standard Version Type
@@ -53,7 +53,7 @@ namespace AutoVersion{
 	static const long BUILDS_COUNT = 1;
 	#define RC_FILEVERSION 0,2,3,8
 	#define RC_FILEVERSION_STRING "0, 2, 3, 8\0"
-	static const char FULLVERSION_STRING[] = "0.2.3.8-in-progress";
+	static const char FULLVERSION_STRING[] = "0.2.3.8";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 33;
