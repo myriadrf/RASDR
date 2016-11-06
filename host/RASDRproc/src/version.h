c3896e87817d433c1ec62c992e74d2c79f6b09c4
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2016-11-06 06:40:37 -0500

    mark experimental branch

--- host/RASDRproc/src/version.h
+++ host/RASDRproc/src/version.h
@@ -35,25 +35,25 @@ namespace AutoVersion{
 
 	//Date Version Types
 	static const char DATE[] = "10";
-	static const char MONTH[] = "22";
+	static const char MONTH[] = "27";
 	static const char YEAR[] = "2016";
 	static const char UBUNTU_VERSION_STYLE[] = "13.10";
 
 	//Software Status
-	static const char STATUS[] = "Alpha";
+	static const char STATUS[] = "Experimental";
 	static const char STATUS_SHORT[] = "";
 
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 2;
-	static const long BUILD = 3;
-	static const long REVISION = 8;
+	static const long BUILD = 4;
+	static const long REVISION = 0;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 1;
-	#define RC_FILEVERSION 0,2,3,8
-	#define RC_FILEVERSION_STRING "0, 2, 3, 8\0"
-	static const char FULLVERSION_STRING[] = "0.2.3.8";
+	#define RC_FILEVERSION 0,2,4,0
+	#define RC_FILEVERSION_STRING "0, 2, 4, 0\0"
+	static const char FULLVERSION_STRING[] = "0.2.4.x-proj_ogl";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 33;
