c7674ad09b46bfdeb5db34976d3e12b490eb68b0
Bogdan Vacaliuc <vacaliucb@ornl.gov>
2016-11-28 20:04:58 -0500

    switch to argparse to prepare to use Gooey

--- host/plotcsv/plotcsv.py
+++ host/plotcsv/plotcsv.py
@@ -1,11 +1,11 @@
-#import math import time
+from __future__ import print_function
 import sys
 import datetime
 import logging
 import numpy as np
 from StringIO import StringIO
 
-DEF_VERSION = '1.2.2.6-dev'     # x.y.z.* to match RASDRviewer version
+DEF_VERSION = '1.2.2.7-dev'     # x.y.z.* to match RASDRviewer version
 DEF_DELIM   = ','
 DEF_AVERAGE = 1
 DEF_CALIB   = 0.0           # Paul uses (0.73278^2)/2000 as Qstep^2/ADC impedance
@@ -619,63 +619,68 @@ def dump_spectrum_info(filename,opts):
         np.savetxt(opts.statistics,sts,fmt=DEF_STATS_FORMAT,delimiter=DEF_STATS_DELIM,header=DEF_STATS_HEADER,comments='')
 
 if __name__ == '__main__':
-    from optparse import OptionParser
-
-    p = OptionParser(version=DEF_VERSION)
-    p.set_usage('plotcsv.py <filename.csv> [options]')
-    p.set_description(__doc__)
+    import argparse
+                    
+    p = argparse.ArgumentParser(
+        description='Post-Process RASDRviewer/RASDRproc spectrum data output files')
+    p.add_argument(      "--version", action='version', version='%(prog)s '+DEF_VERSION)
+    g = p.add_mutually_exclusive_group()
+    g.add_argument("-v", "--verbose", default=False, help="Turn on verbose output", action="store_true")
+    g.add_argument("-q", "--quiet", default=False, help='Suppress progress messages', action="store_true")
     # call matplotlib.use() only once
     p.set_defaults(matplotlib_use = False)
-    p.add_option('-a', '--average', dest='average', type='int', metavar='N', default=DEF_AVERAGE,
-        help='Specify the number of spectra to average for each plot; default=%default')
-    p.add_option('-b', '--background', dest='background', type='str', metavar='PATH', default='',
+    p.add_argument('-a', '--average', type=int, metavar='N', default=DEF_AVERAGE,
+        help='Specify the number of spectra to average for each plot')
+    p.add_argument('-b', '--background', type=str, metavar='PATH', default='',
         help='Specify how to perform background subtraction;'+
              'if the word automatic is used, then the background will be taken'+
              'from the average of all lines in the file.  Otherwise, it is taken'+
              'as a file to process.  The file must have the same frequency plan as the foreground file.')
-    p.add_option('-c', '--cancel-dc', dest='canceldc', action='store_true', default=False,
+    p.add_argument('-c', '--cancel-dc', dest='canceldc', action='store_true', default=False,
         help='Cancel out component at frequency bin for 0Hz')
-    p.add_option('-d', '--delimiter', dest='delimiter', type='str', metavar='CHAR', default=DEF_DELIM,
-        help='Specify the delimiter character to use; default="%default"')
-    p.add_option('-e', '--excel', '--localtime', dest='localtime', action='store_true', default=False,
+    p.add_argument('-d', '--delimiter', type=str, metavar='CHAR', default=DEF_DELIM,
+        help='Specify the delimiter character to use"')
+    p.add_argument('-e', '--localtime', action='store_true', default=False,
         help='Indicate that .csv file has timestamps in RASDRviewer\'s "LocalTime" format')
-    p.add_option('-k', '--calibration', dest='calibration', type='float', metavar='CONST', default=DEF_CALIB,
-        help='Specify the calibration constant for the system; 0.0=uncal, default=%default')    #default=%f'%DEF_CALIB)
-    p.add_option('-l', '--line', dest='line', action='store_true', default=False,
+    p.add_argument('-k', '--calibration', type=float, metavar='CONST', default=DEF_CALIB,
+        help='Specify the calibration constant for the system; 0.0=uncal')    #default=%f'%DEF_CALIB)
+    p.add_argument('-l', '--line', action='store_true', default=False,
         help='Perform line-by-line processing instead of loading entire file(s); NOTE: much slower but tolerates low memory better.')
-##    p.add_option('-m', '--milliwatt', dest='dbm', action='store_true', default=False,
+##    p.add_argument('-m', '--milliwatt', dest='dbm', action='store_true', default=False,
 ##        help='Plot in decibels referenced to 1mW (dBm/Hz)')
-##    p.add_option('-t', '--datetime', dest='datetime', action='store_true', default=False,
+##    p.add_argument('-t', '--datetime', action='store_true', default=False,
 ##        help='Indicate that timestamps in the .csv file are in Excel\'s datetime format')
-    p.add_option('-i', '--info', dest='info', action='store_true', default=False,
+    p.add_argument('-i', '--info', action='store_true', default=False,
         help='Produce information about a file only; do not generate any plots')
-    p.add_option('--statistics', dest='statistics', type='str', metavar='PATH', default=None,
-        help='Dump statistical information to a file in comma-separated-values format, default=%default')
-    p.add_option('-v', '--verbose', dest='verbose', action='store_true', default=False,
-        help='Verbose')
-    p.add_option('--debug', dest='debug', action='store_true', default=False,
+    p.add_argument('--statistics', type=str, metavar='PATH', default=None,
+        help='Dump statistical information to a file in comma-separated-values format')
+    p.add_argument('--debug', action='store_true', default=False,
         help='Drop into ipython shell at predefined point(s) to debug the script')
-    p.add_option('-g', '--gui', dest='gui', action='store_true', default=False,
+    p.add_argument('-g', '--gui', action='store_true', default=False,
         help='Create interactive PLOTS')
-    p.add_option('-s', '--smooth', dest='smooth', type='int', metavar='N', default=0,
+    p.add_argument('-s', '--smooth', type=int, metavar='N', default=0,
         help='Smooth final plot using a sliding window of N points')
-    p.add_option('--fcenter', dest='fc', type='float', default=0.0,
-        help='Define the offset for the center frequency in Hz; default=%default')  #default=%f'%0.0)
-    p.add_option('--hold', dest='hold', action='store_true', default=False,
+    p.add_argument('--fcenter', dest='fc', type=float, default=0.0,
+        help='Define the offset for the center frequency in Hz')  #default=%f'%0.0)
+    p.add_argument('--hold', action='store_true', default=False,
         help='Perform a maximum value HOLD during averaging and plot it as a second line')
-    p.add_option('--bplot', dest='bplot', action='store_true', default=False,
-        help='If using background file, choose whether to plot the background reference in a difffert color; default=%default')
-    p.add_option('--ptype', dest='ptype', type='str', metavar='TYPE', default='log',
-        help='Control plot vertical scale (linear or log); default=%default')
-    p.add_option('--atype', dest='atype', type='str', metavar='TYPE', default='log',
-        help='Control averaging method (linear or log); default=%default')
+    p.add_argument('--bplot', action='store_true', default=False,
+        help='If using background file, choose whether to plot the background reference in a difffert color')
+    p.add_argument('--ptype', type=str, metavar='TYPE', default='log',
+        help='Control plot vertical scale (linear or log)')
+    p.add_argument('--atype', type=str, metavar='TYPE', default='log',
+        help='Control averaging method (linear or log)')
         # http://www.dtic.mil/dtic/tr/fulltext/u2/657404.pdf
     ## for handling RASDRviewer versions
     v = DEF_VERSION.split('.')
     ver = v[0]+'.'+v[1]+'.'+v[2]
-    p.add_option('--format', dest='format', type='str', metavar='X.Y.Z', default=ver,
-        help='Specify the RASDRviewer .csv output format to interpret; default=%s'%ver)
-    opts, args = p.parse_args(sys.argv[1:])
+    p.add_argument('--format', type=str, metavar='X.Y.Z', default=ver,
+        help='Specify the RASDRviewer .csv output format to interpret')
+    # http://stackoverflow.com/questions/20165843/argparse-how-to-handle-variable-number-of-arguments-nargs
+    p.add_argument(      "file", nargs='*')
+
+    opts = p.parse_args(sys.argv[1:])
+    args = opts.file
 
     logging.basicConfig(format='%(message)s',level=logging.DEBUG)
     if args==[]:
@@ -703,7 +708,7 @@ if __name__ == '__main__':
                 generate_spectrum_plots(args[0],opts)
             if opts.gui:
                 from pylab import show
-                print '=== Display interactive graphs ==='
+                print('=== Display interactive graphs ===')
                 show()
         except Exception, e:
             logger.error('generate_spectrum_plots', exc_info=True)
