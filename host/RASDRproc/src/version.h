69caa732837fc7448c14df27540812e275aa4179
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2017-03-30 21:30:57 -0400

    identify release 0.2.5.4

--- host/RASDRproc/src/version.h
+++ host/RASDRproc/src/version.h
@@ -34,7 +34,7 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "02";
+	static const char DATE[] = "22";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2017";
 	static const char UBUNTU_VERSION_STYLE[] = "13.10";
@@ -47,13 +47,13 @@ namespace AutoVersion{
 	static const long MAJOR = 0;
 	static const long MINOR = 2;
 	static const long BUILD = 5;
-	static const long REVISION = 3;
+	static const long REVISION = 4;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 1;
-	#define RC_FILEVERSION 0,2,5,3
-	#define RC_FILEVERSION_STRING "0, 2, 5, 3\0"
-	static const char FULLVERSION_STRING[] = "0.2.5.3-inprogress";
+	#define RC_FILEVERSION 0,2,5,4
+	#define RC_FILEVERSION_STRING "0, 2, 5, 4\0"
+	static const char FULLVERSION_STRING[] = "0.2.5.4";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 33;
